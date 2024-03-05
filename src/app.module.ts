7b46132508a9dc4d3f87f0903746c39488275f27
Ed_sh <edsghot@gmail.com>
2024-03-04 19:51:50 -0500

    se agrego la conexion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'ccontrolz.com',
+    port: 3306,
+    username: 'nibcqvah_edsghot',
+    password: 'Repro123.',
+    database: 'nibcqvah_AppFireSystemDb',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true
+})],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
